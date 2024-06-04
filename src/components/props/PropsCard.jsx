f746f986d9d428f0b9ed9f0df22d80974a0df55f
Yash Rajput <rajputyash8561@gmail.com>
2024-06-04 23:23:44 +0530

    props practice

--- src/components/props/PropsCard.jsx
+++ src/components/props/PropsCard.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-function PropsCard(){
-
+function PropsCard({image, name, profession, friend}){
+// const {name, profession, img} = data;
     return(
-        {}
+        
+            <div className="w-[200px] bg-zinc-300 m-5 rounded-md">
+                <div className="w-full h-32 bg-blue-400 rounded-md  overflow-hidden">
+                <img src={image} alt="" />
+            </div>
+            <div className='w-full p-3 flex justify-between'>
+                <div>
+                <h4 className='font-semibold text-xl'>{name}</h4>
+                <p className='text-sm'>{profession}</p>
+                </div>
+
+                <button className='bg-blue-400 px-3 rounded-md text-sm font-semibold mt-2 text-white'>Add Friend</button>
+            </div>
+            </div>
     )
 }
 
