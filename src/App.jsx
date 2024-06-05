e77250e7bc20739a1b35ff9dfdeb03e3b97d6101
Yash Rajput <rajputyash8561@gmail.com>
2024-06-05 22:00:00 +0530

    handle friends button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./components/Card";
 import State from "./components/State";
 import Addproperty from "./components/Addproperty";
@@ -14,9 +14,24 @@ function App(){
     {name: "Yash",  friend: false, profession: "Editor", img: "https://media.istockphoto.com/id/908231976/photo/bearded-young-man-with-hat.webp?b=1&s=170667a&w=0&k=20&c=jM2iPw2Qe1HZ0_vYAaTYSPRTTizm8-SwZVDg-sNRXOo="},
     {name: "Ravi",  friend: false, profession: "Coder", img: "https://media.istockphoto.com/id/1447861263/photo/smiling-handsome-young-indian-man-wearing-white-casual-t-shirt-and-jeans-isolated-on-beige.webp?b=1&s=170667a&w=0&k=20&c=LrU0W4yKSXS5CY-6NTjvKlWppVGp2cOngkMm7VjXVZY="},
     {name: "Ram",   friend: false, profession: "Crickter", img: "https://media.istockphoto.com/id/1297832726/photo/portrait-of-a-smiling-young-businessman.webp?b=1&s=170667a&w=0&k=20&c=-bG-98gM7AR79cs9Fe7UaRGZnENYooOdSkHF5QkZ7xo="},
-    {name: "Urvi",  friend: false, profession: "influencer", img: "https://media.istockphoto.com/id/1286408574/photo/young-woman-facial-expression-stock-photo.webp?b=1&s=170667a&w=0&k=20&c=bjzCX1Z6MKxKoVZQzk9UhbCOiKyZBsWzwosSdY5wcbw="},
-    {name: "Nishu", friend: false, profession: "Artist", img: "https://media.istockphoto.com/id/1221041044/photo/teenage-girl-at-white-background-stock-images.webp?b=1&s=170667a&w=0&k=20&c=0Gy4mH-Gobn1EeqlVOPicXxWQtl3Y9IhVrumvlxFZKk="},
+    {name: "Sohit",  friend: false, profession: "influencer", img: "https://media.istockphoto.com/id/1286408574/photo/young-woman-facial-expression-stock-photo.webp?b=1&s=170667a&w=0&k=20&c=bjzCX1Z6MKxKoVZQzk9UhbCOiKyZBsWzwosSdY5wcbw="},
+    {name: "Vasu", friend: false, profession: "Artist", img: "https://media.istockphoto.com/id/1221041044/photo/teenage-girl-at-white-background-stock-images.webp?b=1&s=170667a&w=0&k=20&c=0Gy4mH-Gobn1EeqlVOPicXxWQtl3Y9IhVrumvlxFZKk="},
   ]
+
+  const [realDetails, setRealDetails] = useState(details);
+  // console.log(realDetails);
+  const handlebuttonClick = (btnIndex)=>{
+    // alert(" Button clicked ")
+    setRealDetails((prev)=>{
+      return prev.map((item, index)=>{
+        if(index === btnIndex){
+          return {...item, friend: !item.friend}
+        }
+        return item;
+      })
+    
+    })
+  }
 
   return (
     // <> this sign called fragments, without creating a new div we can return multiple components
@@ -31,8 +46,8 @@ function App(){
         {/* < Props text="Know More" />
         < Props text="Download"/> */}
         <div className="w-full h-screen flex justify-center items-center flex-wrap">
-        {details.map((item, index)=> (
-        < PropsCard image={item.img} name={item.name} profession={item.profession} friend={item.friend}/>)
+        {realDetails.map((item, index)=> (
+          < PropsCard key={index} index={index} values={item} handleClick={handlebuttonClick}/>)
         )}
         </div>
     </>
--- src/components/props/PropsCard.jsx
+++ src/components/props/PropsCard.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function PropsCard({image, name, profession, friend}){
-// const {name, profession, img} = data;
-    return(
-        
-            <div className="w-[200px] bg-zinc-300 m-5 rounded-md">
-                <div className="w-full h-32 bg-blue-400 rounded-md  overflow-hidden">
-                <img src={image} alt="" />
-            </div>
-            <div className='w-full p-3 flex justify-between'>
-                <div>
-                <h4 className='font-semibold text-xl'>{name}</h4>
-                <p className='text-sm'>{profession}</p>
-                </div>
+function PropsCard({ values, handleClick, index }) {
+  // console.log(values);
+    const {img, name, profession, friend} = values;
+  return (
+    <div className="w-[200px] bg-zinc-300 m-5 rounded-md">
+      <div className="w-full h-32 bg-blue-400 rounded-md  overflow-hidden">
+        <img src={img} alt="" />
+      </div>
+      <div className="w-full p-3 flex justify-between">
+        <div>
+          <h4 className="font-semibold text-xl">{name}</h4>
+          <p className="text-sm">{profession}</p>
+        </div>
 
-                <button className='bg-blue-400 px-3 rounded-md text-sm font-semibold mt-2 text-white'>Add Friend</button>
-            </div>
-            </div>
-    )
+        <button onClick={()=> handleClick(index)} className={`${friend ?  "bg-green-400" : "bg-blue-400"} px-3 rounded-md text-sm font-semibold mt-2 text-white`}>
+          {friend ? "Friends" : "Add Friend"}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default PropsCard;
