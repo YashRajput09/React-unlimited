a957f9c94475894e650acdd09c015810301d7ca0
Yash Rajput <rajputyash8561@gmail.com>
2024-06-19 16:09:02 +0530

    handle form data and functionality of remove it buttom

--- src/components/FormHandling/formData/FHParentComponent.jsx
+++ src/components/FormHandling/formData/FHParentComponent.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cards from './formActivity/Cards';
 import Form from './formActivity/Form';
 
 function FHControledComponent(){
+    const [users, setUsers] = useState([]);
+    const handleFormData = (data) =>{
+        setUsers([...users, data]);
+    }
+
+    const handleRemoveBtn = (id)=>{
+        setUsers((prev) => {
+        return prev.filter((item, index) =>{
+            return index !== id;
+        })
+    })
+    }
     return(
-        <div className='w-full h-screen bg-zinc-200 flex justify-center items-center'>
+        <div className='w-full h-screen bg-zinc-200 flex flex-col justify-center items-center'>
             <div className="mx-12 rounded-lg">
-            < Cards />
-            < Form />
+            < Cards users={users} handleRemoveBtn={handleRemoveBtn}/>
+            < Form handleFormData={handleFormData} />
             </div>
         </div>
     )
--- src/components/FormHandling/formData/formActivity/Cards.jsx
+++ src/components/FormHandling/formData/formActivity/Cards.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Card from './Card';
 
-function Cards(){
-    return(
-        <div className='px-10w-full max-h-72 overflow-auto flex justify-center items-center p-2 gap-4 flex-wrap'>
-           < Card />
-        </div>
+function Cards({users, handleRemoveBtn }){    
+    return( 
+      <div className='px-10w-full max-h-72 overflow-auto flex justify-center items-center p-2 gap-4 flex-wrap'>
+        {users.map((item, index) =>{
+           return < Card user={item} key={index} id={index} handleRemoveBtn={handleRemoveBtn}/>
+        })}
+    </div>
     )
 }
 
